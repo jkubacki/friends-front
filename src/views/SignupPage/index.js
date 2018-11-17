44903e1e4ce31fb218a337b5a524898e620d8231
Jakub Kubacki <kubacki.jk@gmail.com>
2018-11-17 20:38:59 +0700

    Add google login on signup page

--- src/views/SignupPage/index.js
+++ src/views/SignupPage/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { GoogleLogin } from 'react-google-login';
 
 import SignupForm from 'components/SignupForm'
+import { socialLogin } from 'actions/auth';
+import { extractProps } from 'utils';
 
 class SignupPage extends React.Component {
   render() {
+    const { googleLogin } = this.props;
+    const {
+      REACT_APP_GOOGLE_CLIENT_ID,
+      REACT_APP_GOOGLE_OAUTH_SCOPES,
+    } = process.env;
+
     return (
       <div>
         SignupPage
         <SignupForm />
+        <GoogleLogin
+          clientId={REACT_APP_GOOGLE_CLIENT_ID}
+          responseType="code"
+          scope={REACT_APP_GOOGLE_OAUTH_SCOPES}
+          onSuccess={googleLogin}
+          tag="span"
+        />
       </div>
     );
   }
@@ -19,7 +35,11 @@ class SignupPage extends React.Component {
 export default compose(
   connect(
     null,
-    {},
+    { socialLogin },
   ),
   withRouter,
+  extractProps(props => ({
+    googleLogin: ({ code }) =>
+      props.socialLogin({ provider: 'google', assertion: code }),
+  })),
 )(SignupPage);
